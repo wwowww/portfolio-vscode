5ae38137742caa2d58243a6b87c2278d8cd7fcd8
wwowww <105021733+wwowww@users.noreply.github.com>
2024-09-06 20:03:25 +0900

    feat. typing component 추가

---
+++ src/components/atoms/TypingText/TypingText.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useRef, useState } from "react";
+
+const TypingText = ({text, frame}: any) => {
+    const [title, setTitle] = useState('');
+    const [count, setCount] = useState(0);
+    const completionWord = text;
+
+  useEffect(() => {
+    const typingInterval = setInterval(() => {
+      setTitle((prevTitleValue) => {
+        let result = prevTitleValue ? prevTitleValue + completionWord[count] : completionWord[0];
+        setCount(count + 1);
+
+        if (count >= completionWord.length) return title;
+
+        return result;
+      });
+    }, frame);
+
+    return () => clearInterval(typingInterval);
+  });
+
+  return <span>{title}</span>;
+};
+
+export default TypingText;
