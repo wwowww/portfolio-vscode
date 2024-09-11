9a73b4f7d9f75be1957f44ecdc56b09169ab3cf5
wwowww <105021733+wwowww@users.noreply.github.com>
2024-09-11 20:59:54 +0900

    docs. TypographyCode 추가

--- src/types/Typography.ts
+++ src/types/Typography.ts
@@ -3,4 +3,5 @@ export type TypographyVariants = "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "p" |
 export type TypographyCode = 
 "title-40-140-600"
 | "title-14-140-400"
-| "body-24-140-600";
+| "body-24-140-600"
+| "body-18-150-400";
