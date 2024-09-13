f4cc32e073e21b6b38ae7eb59131fceeecddb4ea
wwowww <105021733+wwowww@users.noreply.github.com>
2024-09-13 14:09:32 +0900

    feat. About 추가

--- src/components/atoms/CodeStyle/CodeLine/CodeLine.tsx
+++ src/components/atoms/CodeStyle/CodeLine/CodeLine.tsx
@@ -2,17 +2,37 @@ import { ReactNode } from "react";
 import style from "./CodeLine.module.scss";
 
 type Props = {
-  tag?: string;
+  htmlTag?: string;
+  tagClose?: boolean;
+  attribute?: string;
+  attributeContent?: string;
+  className?: string;
   bracket?: 'open' | 'close';
+  angleBracket?: 'open' | 'close';
   children?: ReactNode;
   indent?: number;
 }
 
-const CodeLine = ({tag, bracket, children, indent}: Props) => {
+const CodeLine = ({htmlTag, tagClose, attribute, attributeContent, className, bracket, angleBracket, children, indent}: Props) => {
   return (
     <div className={style.wrap}>
-      {tag && <span className={`${style.normal} ${style[tag]}`} key={tag}>.{tag}</span>}
+      {className && <span className={`${style.normal} ${style[className]}`} key={className}>.{className}</span>}
+      {htmlTag && 
+        <span className={`${style.htmlTag} ${style[`indent-`+indent]}`}>
+          {tagClose && <span className={style.tagClose}>/</span>}
+          {htmlTag}
+          {
+            attribute && 
+              <span className={style.attribute}>{' '}{attribute}
+                <span className={style.equalSign}>=</span>
+                <span className={style.content}>"{attributeContent}"</span>
+              </span>
+          }
+        </span>
+      }
       {bracket === "open"  && " {"}
+      {angleBracket === "open" && "<"}
+      {angleBracket === "close" && ">"}
       {children && <span className={style[`indent-`+indent]}>{children}</span>}
       {bracket === "close"  && "}"}
     </div>
---
+++ src/pages/About.tsx
@@ -0,0 +1,38 @@
+import CodeBlock from "@/components/atoms/CodeStyle/CodeBlock/CodeBlock";
+import CodeLine from "@/components/atoms/CodeStyle/CodeLine/CodeLine";
+
+import style from "@/assets/styles/About.module.scss";
+
+const About = () => {
+  return (
+    <>
+      <CodeBlock>
+        <CodeLine htmlTag="html" attribute="lang" attributeContent="ko" />
+        <CodeLine htmlTag="head" indent={2} />
+          <CodeLine htmlTag="title" indent={4} />
+          <CodeLine indent={6}>안녕하세요. 프론트엔드 개발자 권은지입니다.</CodeLine>
+          <CodeLine htmlTag="title" indent={4} tagClose />
+        <CodeLine htmlTag="head" indent={2} tagClose />
+        <CodeLine htmlTag="body" indent={2} />
+        <CodeLine htmlTag="header" indent={4} attribute="class" attributeContent="information"  />
+
+        <CodeLine htmlTag="header" indent={4} tagClose />
+        <CodeLine htmlTag="main" indent={4} attribute="class" attributeContent="skills" />
+        <CodeLine indent={6}>경험을 토대로 작성할 것! 프로젝트들</CodeLine>
+
+        <CodeLine htmlTag="main" indent={4} tagClose />
+
+        <CodeLine htmlTag="section" indent={4} attribute="class" attributeContent="career" />
+
+        <CodeLine htmlTag="section" indent={4} tagClose />
+        <CodeLine htmlTag="footer" indent={4} attribute="class" attributeContent="education" />
+
+        <CodeLine htmlTag="footer" indent={4} tagClose />
+        <CodeLine htmlTag="body" indent={2} tagClose />
+        <CodeLine htmlTag="html" tagClose />
+      </CodeBlock> 
+    </>
+  ) 
+}
+
+export default About;
