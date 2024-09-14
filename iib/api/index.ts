0b1ca5d6fd11e21c7d264723b0289c4f5a1b465b
wwowww <105021733+wwowww@users.noreply.github.com>
2024-09-14 21:10:43 +0900

    remove. 불필요한 파일 삭제

--- iib/api/index.ts
+++
@@ -1,7 +0,0 @@
-const jsonUrl = "../../iib/api/projectList.json";
-import axios from "axios";
-
-export const fetchData = async () => {
-  const response = await axios.get(jsonUrl)
-  return response.data.data
-}
