fef2199b3ff53ba5f85c208b11b17b4cd8c10850
wwowww <105021733+wwowww@users.noreply.github.com>
2024-09-16 01:39:34 +0900

    docs. title에 이미지 없을 경우 추가

--- src/components/molecules/SectionTitle/SectionTitle.tsx
+++ src/components/molecules/SectionTitle/SectionTitle.tsx
@@ -11,7 +11,7 @@ const SectionTitle = ({title, imageSrc, className}: Props) => {
   return (
     <div className={style.wrap}>
       <Typography Tag="h2" className="body-24-140-600">
-        <img src={imageSrc} alt={`${className} icon.`} className={style[className]} />
+        {imageSrc && <img src={imageSrc} alt={`${className} icon.`} className={style[className]} />}
         {title}
       </Typography>
     </div>
