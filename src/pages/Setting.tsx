45bdd1604af25a045174613d6b85464353242022
wwowww <105021733+wwowww@users.noreply.github.com>
2024-10-04 18:25:24 +0900

    fix. toString 오류, 이미지 경로 수정

--- src/pages/Setting.tsx
+++ src/pages/Setting.tsx
@@ -2,10 +2,11 @@ import { THEME_LIST } from "@/static/mocks/themeList";
 import style from "@/assets/styles/Setting.module.scss";
 import { useState } from 'react';
 import SectionTitle from '@/components/molecules/SectionTitle/SectionTitle';
+import sectionImage from "@/static/icons/SettingIcon.svg";
 
 const Setting = () => {
   const localTheme = localStorage.getItem("theme") as string;
-  const [themeName, setThemeName] = useState<string>(localTheme.toString());
+  const [themeName, setThemeName] = useState<string>(localTheme);
   
   const setTheme = (theme) => {
     document.documentElement.setAttribute('data-theme', theme);
@@ -16,7 +17,7 @@ const Setting = () => {
   return (
     <>
       <SectionTitle
-        imageSrc="/src/static/icons/SettingIcon.svg"
+        imageSrc={sectionImage}
         title="Setting Color Themes"
         className="cogwheel"
       />
